54e823a7185d562e8e6ec981dd9b2973ca4def34
lilao8 <admin@YGdeMacBook-Pro.local>
2018-04-03 16:29:59 +0800

    hello

---
+++ tonyMa/tonyMa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tonyMa
+//
+//  Created by YG on 2018/4/3.
+//  Copyright © 2018年 chouxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tonyMa/tonyMa/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  tonyMa
+//
+//  Created by YG on 2018/4/3.
+//  Copyright © 2018年 chouxiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
